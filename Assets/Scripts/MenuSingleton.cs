2dee5ab872290afa50920b321fdb1b660afd0508
Steven Weiss <steventrouble@gmail.com>
2021-04-26 13:28:54 -0700

    Add fadeout to menu

--- Assets/Scripts/MenuSingleton.cs
+++ Assets/Scripts/MenuSingleton.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuSingleton : MonoBehaviour
 {
+    [Tooltip("The group that we'll fade to black on menu outro")]
+    public CanvasGroup fadeGroup;
+
+    [Tooltip("Amount (fraction, 1 = 100%) we should fade out per second.")]
+    public float fadeSpeed = 1;
+
+    private bool isFadingToBlack = false;
 
     public void OpenURL(string url)
     {
@@ -16,6 +23,20 @@ public class MenuSingleton : MonoBehaviour
         // TODO: Load the real scene whatever that is
         SceneManager.LoadScene("AudreyScene");
     }
+    
+    public void StartFadeOut()
+    {
+        isFadingToBlack = true;
+    }
+
+    public void FadeOut()
+    {
+        if (fadeGroup.alpha > 0.0f) {
+            fadeGroup.alpha -= fadeSpeed * Time.deltaTime;
+        } else {
+            StartGame();
+        }
+    }
 
     protected void Update()
     {
@@ -23,7 +44,12 @@ public class MenuSingleton : MonoBehaviour
             || Input.GetKeyDown(KeyCode.Return)
             || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
-            StartGame();
+            StartFadeOut();
+        }
+
+        if (isFadingToBlack)
+        {
+            FadeOut();
         }
     }
 }

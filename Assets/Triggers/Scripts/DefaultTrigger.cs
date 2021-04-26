a11712b3f66dfdab9bdf381758eaa63b5a04071f
Steven Weiss <steventrouble@gmail.com>
2021-04-26 14:33:36 -0700

    Allow enabling and disabling triggers
    ... with different sound effects for each.

--- Assets/Triggers/Scripts/DefaultTrigger.cs
+++ Assets/Triggers/Scripts/DefaultTrigger.cs
@@ -14,10 +14,21 @@ public abstract class DefaultTrigger : Trigger
     public bool triggerOnlyOnce = false;
 
     [Tooltip(
+        "Required: The component that will play the audio.")]
+    public AudioSource audioPlayer = null;
+
+    [Tooltip(
         "If present, this trigger will play this audio clip when triggered. To use this, enable" +
-        " the audio source on this object, and add an audio clip to it. Then drag the audio source" +
-        " component here.")]
-    public AudioSource playAudioOnTrigger = null;
+        " the audio source on this object.")]
+    public AudioClip playAudioOnTrigger = null;
+
+    [Tooltip(
+        "If present, this trigger will play these audio clips when triggered. On each use, it will"
+        + " move to the next item in the list.")]
+    public AudioClip[] playAudioOnDisabled = {};
+
+    [Tooltip("Whether the player can interact with this.")]
+    public bool isEnabled = true;
 
     [Tooltip(
         "Define this if you want the Room to be able to identify and react to this trigger.")]
@@ -26,21 +37,27 @@ public abstract class DefaultTrigger : Trigger
     /** If true, this trigger has already been triggered before. */
     private bool triggered = false;
 
+    /** The number of times the player interacted with this trigger when it was disabled. For audio. */
+    private int numTimesDisabled = 0;
+
     /** Actually run the trigger logic. */
     override protected void OnTrigger() {
         if (triggerOnlyOnce && triggered) {
             Debug.Log("DefaultTrigger.OnTrigger (" + triggerType + "): Ignored because of triggerOnlyOnce.");
             return;
         }
-        triggered =  true;
 
         Debug.Log("DefaultTrigger.OnTrigger (" + triggerType + ")");
 
-        if (playAudioOnTrigger) {
-            playAudioOnTrigger.Play();
+        if (isEnabled && playAudioOnTrigger) {
+            audioPlayer.clip = playAudioOnTrigger;
+            audioPlayer.Play();
+            triggered =  true;
+            RoomArranger.instance.OnTrigger(triggerType);
+        } else if (!isEnabled && playAudioOnDisabled.Length > 0) {
+            audioPlayer.clip = playAudioOnDisabled[numTimesDisabled++ % playAudioOnDisabled.Length];
+            audioPlayer.Play();
         }
-
-        RoomArranger.instance.OnTrigger(triggerType);
     }
 
     // Start is called before the first frame update
